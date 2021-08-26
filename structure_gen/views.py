e04f81d40d9f04aff47e2a041b60aefc4bbcaa05
vishkid84 <vishalittr@gmail.com>
2021-08-26 16:01:29 +0000

    added random in views

--- structure_gen/views.py
+++ structure_gen/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
+from django.core import serializers
+import random
 
-# Create your views here.
+from .models import Story
 
 
 def index(request):
+    stories = list(Story.objects.values())
+    random_story = random.choice(stories)
     template = 'structure_gen/index.html'
     context = {
-        
+        'stories': stories,
+        'random_story': random_story,
     }
 
     return render(request, template, context)

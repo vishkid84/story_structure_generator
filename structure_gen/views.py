c124ca088b1d4b3723a818646ad55fdb6e57ac1d
vishkid84 <vishalittr@gmail.com>
2021-09-01 21:11:21 +0000

    added form

--- structure_gen/views.py
+++ structure_gen/views.py
@@ -3,15 +3,29 @@ from django.core import serializers
 import random
 
 from .models import Story
+from .forms import StoryForm
 
 
-def index(request):
+def home(request):
     stories = list(Story.objects.values())
     random_story = random.choice(stories)
+
+    if request.method == 'POST':
+        form = StoryForm(request.POST or None)
+        if form.is_valid:
+            story = form.save()
+            story.name = random_story.name
+            story.save()
+            return redirect(reverse('home'))
+
+    else:
+        form = StoryForm()
+
     template = 'structure_gen/index.html'
     context = {
         'stories': stories,
         'random_story': random_story,
+        'form': form,
     }
 
     return render(request, template, context)
